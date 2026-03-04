ad74e873c0bcc0e499d56db391cbce9b1ae18bad
Emir Doruk Pırasalar <161344008+emirdoruk@users.noreply.github.com>
2026-03-04 21:32:17 +0300

    Add files via upload

---
+++ LCD_Ekran.ino
@@ -0,0 +1,16 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+                                                                                                                                                          
+LiquidCrystal_I2C lcd(0x27,16,2);                                  
+
+void setup() {
+  Serial.begin(9600);                    
+  lcd.init();
+  lcd.backlight();
+  
+}
+
+void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print("Çankırılı gerçek değil.");
+}
